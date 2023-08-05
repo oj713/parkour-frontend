c40ff4decde9c6fac9c6c7c0f4af3ae381d7e515
bsander15 <bsander1515@gmail.com>
2023-08-05 09:24:47 -0500

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router';
+import Home from './Home';
+import Profile from './Profile';
+import Details from './Details';
+import Results from './Results';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
