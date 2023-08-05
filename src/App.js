e0c751e4d4536bea08b76768dd6d82f9155279b6
bsander15 <bsander1515@gmail.com>
2023-08-05 10:03:39 -0500

    reducer architecture

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { HashRouter } from 'react-router-dom';
-import { Routes, Route, Navigate } from 'react-router';
+import { Routes, Route } from 'react-router';
+import postsReducer from './reducers/posts-reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import Home from './Home';
 import Profile from './Profile';
 import Details from './Details';
 import Results from './Results';
 import Login from './Login';
+const store = configureStore({
+  reducer: { posts: postsReducer }
+});
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </HashRouter>
+    </Provider>
   );
 }
 
---
+++ src/reducers/posts-reducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState: [],
+    reducers: {
+        postlikeToggle(state, action) {
+            const post = state.find(post => post._id === action.payload._id)
+            post.likes = post.liked ? post.likes - 1 : post.likes + 1
+            post.liked = !post.liked
+        },
+        createPost(state, action) {
+            state.unshift({
+              ...action.payload,
+              ...templatepost,
+              _id: (new Date()).getTime(),
+            })
+          },
+          deletepost(state, action) {
+            const index = state.findIndex(post =>
+                  post._id === action.payload);
+            state.splice(index, 1);
+          }
+     }
+});
+
+export default postsSlice.reducer;
+export const { postlikeToggle, createPost, deletepost } = postsSlice.actions;
