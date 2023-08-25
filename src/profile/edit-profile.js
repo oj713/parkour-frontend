f9a95e3126b9a2981efa3a734325b043dd934ced
Joseph Maydwell <maydwell.j@northeastern.edu>
2023-08-25 14:12:05 -0400

    Password save added

--- src/profile/edit-profile.js
+++ src/profile/edit-profile.js
@@ -1,10 +1,54 @@
 import ProfileHeader from './profile-header';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { updateUserThunk, profileThunk, logoutThunk } from "../services/auth-thunks";
 
 const EditProfileScreen = () => {
+    const { currentUser } = useSelector((state) => state.auth);
+    const [profile, setProfile] = useState(currentUser);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const save = async () => { await dispatch(updateUserThunk(profile)) };
+    useEffect(() => {
+        const loadProfile = async () => {
+            const { payload } = await dispatch(profileThunk());
+            setProfile(payload);
+        };
+        loadProfile();
+    }, []);
     return (
         <div>
-            <h2>Edit Profile</h2>
-            {/* Add input fields to edit profile data */}
+            <h1>Profile Screen</h1>
+            {profile && (<div>
+                <div>
+                    <label>Profile Name</label>
+                    <input type="text" value={profile.displayName}
+                        onChange={(event) => {
+                            const newProfile = {
+                                ...profile, displayName: event.target.value,
+                            };
+                            setProfile(newProfile);
+                        }} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="text" value={profile.password}
+                        onChange={(event) => {
+                            const newProfile = {
+                                ...profile, password: event.target.value,
+                            };
+                            setProfile(newProfile);
+                        }} />
+                </div>
+            </div>
+            )}
+            <button
+                onClick={() => {
+                    dispatch(logoutThunk());
+                    navigate("/login");
+                }}>                   Logout</button>
+            <button onClick={save}>Save  </button>
         </div>
     );
 };
